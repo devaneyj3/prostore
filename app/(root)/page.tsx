53683b98d6b20b499349d0d1f857f134bccb546e
devaneyj3 <jordandevaney28@gmail.com>
2025-07-14 21:06:34 -0400

    product list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 const HomePage = () => {
-	return <>Prostore</>;
+	return (
+		<>
+			<ProductList data={sampleData.products} title="New Arrivals" limit={4} />
+		</>
+	);
 };
 
 export default HomePage;
